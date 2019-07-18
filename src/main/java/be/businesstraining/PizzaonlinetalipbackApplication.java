e1eaabb65961a781e944917dd17e937e4884685f
Talip Saritas <talip.saritas@gmail.com>
2019-07-18 11:41:30 +0200

    Commit initial du back

---
+++ src/main/java/be/businesstraining/PizzaonlinetalipbackApplication.java
@@ -0,0 +1,13 @@
+package be.businesstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaonlinetalipbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaonlinetalipbackApplication.class, args);
+    }
+
+}
